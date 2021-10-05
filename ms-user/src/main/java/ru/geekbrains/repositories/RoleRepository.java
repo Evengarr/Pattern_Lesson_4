5971ea060f222cd2130a577df11ddfedc961f0f9
Evengarr <72157480+Evengarr@users.noreply.github.com>
2021-10-05 22:06:57 +0300

    Delete RoleRepository.java

--- ms-user/src/main/java/ru/geekbrains/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.geekbrains.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByName(String name);
-}
